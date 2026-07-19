11eccc363e51e5cae1bc1f4968d2f3e698234268
Keeru <keerusandra70@gmail.com>
2026-07-19 20:30:20 +0300

    Update project structure

--- main.py
+++ main.py
@@ -2,116 +2,154 @@ import argparse
 
 from rich import print
 
+from models.user import User
 from models.project import Project
 from models.task import Task
-from models.user import User
 from utils.storage import load_data, save_data
+from utils.helpers import find_user, find_project, find_task
 
 
-users = load_data()
+def main():
+    users = load_data()
 
-parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="Project Tracker CLI"
+    )
 
-sub = parser.add_subparsers(dest="command")
+    subparsers = parser.add_subparsers(dest="command")
 
+    add_user = subparsers.add_parser("add-user")
+    add_user.add_argument("--name", required=True)
+    add_user.add_argument("--email", required=True)
 
-add_user = sub.add_parser("add-user")
-add_user.add_argument("--name", required=True)
-add_user.add_argument("--email", required=True)
+    subparsers.add_parser("list-users")
 
+    add_project = subparsers.add_parser("add-project")
+    add_project.add_argument("--user", required=True)
+    add_project.add_argument("--title", required=True)
+    add_project.add_argument("--description", default="")
+    add_project.add_argument("--due_date", default="")
 
-list_users = sub.add_parser("list-users")
+    list_projects = subparsers.add_parser("list-projects")
+    list_projects.add_argument("--user", required=True)
 
+    add_task = subparsers.add_parser("add-task")
+    add_task.add_argument("--user", required=True)
+    add_task.add_argument("--project", required=True)
+    add_task.add_argument("--title", required=True)
 
-add_project = sub.add_parser("add-project")
-add_project.add_argument("--user", required=True)
-add_project.add_argument("--title", required=True)
-add_project.add_argument("--description", default="")
-add_project.add_argument("--due_date", default="")
+    complete_task = subparsers.add_parser("complete-task")
+    complete_task.add_argument("--user", required=True)
+    complete_task.add_argument("--project", required=True)
+    complete_task.add_argument("--title", required=True)
 
+    args = parser.parse_args()
 
-list_projects = sub.add_parser("list-projects")
-list_projects.add_argument("--user", required=True)
+    if args.command == "add-user":
 
+        if find_user(users, args.name):
+            print("[red]User already exists.[/red]")
+            return
 
-add_task = sub.add_parser("add-task")
-add_task.add_argument("--project", required=True)
-add_task.add_argument("--title", required=True)
+        user = User(args.name, args.email)
+        users.append(user)
+        save_data(users)
 
+        print(f"[green]User '{args.name}' added successfully![/green]")
 
-complete = sub.add_parser("complete-task")
-complete.add_argument("--project", required=True)
-complete.add_argument("--title", required=True)
+    elif args.command == "list-users":
 
+        if not users:
+            print("[yellow]No users found.[/yellow]")
+            return
 
-args = parser.parse_args()
+        for user in users:
+            print(user)
 
+    elif args.command == "add-project":
 
-if args.command == "add-user":
+        user = find_user(users, args.user)
 
-    users.append(User(args.name, args.email))
-    save_data(users)
-    print("[green]User added[/green]")
+        if not user:
+            print("[red]User not found.[/red]")
+            return
 
+        project = Project(
+            args.title,
+            args.description,
+            args.due_date
+        )
 
-elif args.command == "list-users":
+        user.add_project(project)
 
-    for user in users:
-        print(user)
+        save_data(users)
 
+        print(f"[green]Project '{args.title}' added to {user.name}.[/green]")
 
-elif args.command == "add-project":
+    elif args.command == "list-projects":
 
-    for user in users:
+        user = find_user(users, args.user)
 
-        if user.name == args.user:
-            user.add_project(
-                Project(
-                    args.title,
-                    args.description,
-                    args.due_date
-                )
-            )
+        if not user:
+            print("[red]User not found.[/red]")
+            return
 
-            save_data(users)
-            print("[green]Project added[/green]")
-            break
-
-
-elif args.command == "list-projects":
-
-    for user in users:
-
-        if user.name == args.user:
-
-            for project in user.projects:
-                print(project)
-
-
-elif args.command == "add-task":
-
-    for user in users:
-
-        for project in user.projects:
-
-            if project.title == args.project:
-
-                project.add_task(Task(args.title))
-                save_data(users)
-                print("[green]Task added[/green]")
-
-
-elif args.command == "complete-task":
-
-    for user in users:
+        if not user.projects:
+            print("[yellow]No projects found.[/yellow]")
+            return
 
         for project in user.projects:
+            print(project)
 
-            if project.title == args.project:
+    elif args.command == "add-task":
 
-                for task in project.tasks:
+        user = find_user(users, args.user)
 
-                    if task.title == args.title:
-                        task.complete()
-                        save_data(users)
-                        print("[green]Task completed[/green]")
+        if not user:
+            print("[red]User not found.[/red]")
+            return
+
+        project = find_project(user, args.project)
+
+        if not project:
+            print("[red]Project not found.[/red]")
+            return
+
+        project.add_task(Task(args.title))
+
+        save_data(users)
+
+        print(f"[green]Task '{args.title}' added successfully.[/green]")
+
+    elif args.command == "complete-task":
+
+        user = find_user(users, args.user)
+
+        if not user:
+            print("[red]User not found.[/red]")
+            return
+
+        project = find_project(user, args.project)
+
+        if not project:
+            print("[red]Project not found.[/red]")
+            return
+
+        task = find_task(project, args.title)
+
+        if not task:
+            print("[red]Task not found.[/red]")
+            return
+
+        task.complete()
+
+        save_data(users)
+
+        print(f"[green]Task '{args.title}' marked as completed.[/green]")
+
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
